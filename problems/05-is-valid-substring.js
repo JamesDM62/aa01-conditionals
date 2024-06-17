ca084527f5dba8c64ce6e94fdecf56e86cc06c52
JamesDM62 <lsufan622000@yahoo.com>
2024-06-17 00:36:09 -0500

    practice

--- problems/05-is-valid-substring.js
+++ problems/05-is-valid-substring.js
@@ -5,12 +5,18 @@ first string regardless of the casing of the characters. Otherwise it should
 return "INVALID".
 */
 
-// Your code here 
+function isValidSubStr(str1, str2) {
+    if (str1.toLowerCase().includes(str2.toLowerCase())) {
+        return "VALID";
+    } else {
+        return "INVALID";
+    }
+}
 
-// console.log(isValidSubStr("JOY", "joy"));                     // => 'VALID'
-// console.log(isValidSubStr("The cat jumped!", "he cat jump")); // => 'VALID'
-// console.log(isValidSubStr("Time to program", "time"));        // => 'VALID'
-// console.log(isValidSubStr("happy", "happiness"));             // => 'INVALID'
+console.log(isValidSubStr("JOY", "joy"));                     // => 'VALID'
+console.log(isValidSubStr("The cat jumped!", "he cat jump")); // => 'VALID'
+console.log(isValidSubStr("Time to program", "time"));        // => 'VALID'
+console.log(isValidSubStr("happy", "happiness"));             // => 'INVALID'
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = isValidSubStr;
